194c1e7758bc673727e8d8f2f11548b910d0a24e
Keith Wright <Keith Wright>
2016-09-16 12:43:55 +0100

    inital commit

---
+++ vanilla-fontspy.js
@@ -0,0 +1,95 @@
+function fontSpy(fontName, callback, options) {
+    // Throw error if fontName is not a string or not is left as an empty string
+    if (Object.prototype.toString.call(fontName) === "[object Array]") {
+        // we have an array, thats ok
+    } else if (fontName && typeof fontName === "string") {
+        // we have a string, convert it to an array
+        fontName = [fontName];
+    } else {
+        throw "A valid fontName is required. fontName must be a string or an array.";
+    }
+
+    // defaults
+    options = options || {};
+    options.testFont = options.testFont || "Courier New";
+    options.testString = options.testString || "QW@HhsXJIO";
+    options.glyphs = options.glyphs || "";
+    options.delay = options.delay || 100;
+    options.timeOut = options.timeOut || 1000;
+
+    // lets get at it :)
+    var testElements = {};
+
+    for (var i = 0; i < fontName.length; i++) {
+        var testElement = document.createElement("span");
+        testElement.style.position = "absolute";
+        testElement.style.top = "-9999px";
+        testElement.style.left = "-9999px";
+        testElement.style.visibility = "hidden";
+        testElement.style.fontFamily = options.testFont;
+        testElement.style.fontSize = options.fontSize;
+        testElement.innerHTML = options.testString + options.glyphs;
+
+        document.body.appendChild(testElement);
+
+        testElements[fontName[i]] = {
+            element: testElement,
+            fallbackFontWidth: testElement.offsetWidth,
+            hasChangedWidth: function () {
+                return this.fallbackFontWidth !== this.element.offsetWidth
+            }
+        }
+
+        testElement.style.fontFamily = fontName[i].split(",")[0] + "," + options.testFont
+    }
+
+    var failure = function () {
+        var loadedFonts = [];
+        for (var i = 0; i < fontName.length; i++) {
+            var testElement = testElements[fontName[i]];
+            if (testElement) {
+                document.body.removeChild(testElement.element);
+                testElement = undefined;
+            } else {
+                loadedFonts.push(fontName[i]);
+            }
+        }
+
+        callback(loadedFonts);
+    };
+
+    var success = function () {
+        callback(fontName);
+    };
+
+    var retry = function () {
+        if (options.timeOut > 0) {
+            setTimeout(checkFont, options.delay);
+            options.timeOut -= options.delay;
+        } else {
+            failure();
+        }
+    };
+
+    var checkFont = function () {
+        var allFontsLoaded = true;
+
+        for (var i = 0; i < fontName.length; i++) {
+            var testElement = testElements[fontName[i]];
+            if (testElement && testElement.hasChangedWidth()) {
+                document.body.removeChild(testElement.element);
+                testElement = undefined;
+            } else {
+                allFontsLoaded = false;
+            }
+        }
+
+        if (allFontsLoaded) {
+            success();
+        } else {
+            retry();
+        }
+    }
+
+    checkFont();
+};
